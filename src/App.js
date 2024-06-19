6638161ec6168914a1f071016fa2baac533fc9a8
saranyasri <dentist.apassionateprogrammer@gmail.com>
2024-06-19 18:54:29 +0530

    added header with save button and main with react flow with background md copntrols

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import react from "react";
-function App(){
-  return <>
-  <p> Happy starting the assignment</p></>
+import ReactFlow, { Controls, Background } from 'reactflow';
+import 'reactflow/dist/style.css';
+import classes from "./App.module.css"
+function App() {
+  const handleSaveFlow=()=>{
+
+  }
+  return (
+    <div className={classes.App}>
+      <header className={classes.header}> 
+        <button onClick={handleSaveFlow} style={{color:"blue", background:"white", border:"1px solid blue", borderRadius:"10px",padding:"10px 30px"}}className={classes.button}>Save changes</button>
+      </header>
+
+ <main className={classes.reactflow} style={{ height: '80vh', width:"80%" }}>
+   <ReactFlow>
+    <Background/>
+    <Controls/>
+   </ReactFlow>
+       </main>
+    </div>
+   
+  );
 }
-export default App
+
+export default App;
